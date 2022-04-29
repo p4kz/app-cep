f176c1bd73747698e059457fa47917701a910b68
p4kz <wolpak@hotmail.com>
2022-04-29 03:06:33 -0300

    created visual errors

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,12 @@
 const textUser = document.querySelector('.text-user')
 const result = document.querySelector('.result-input')
+const cepInvalid = document.querySelector('.cep-invalid') 
+const cepLength = document.querySelector('.cep-length')
+const cepExistent = document.querySelector('.cep-existent')
 
 function getCep() {
-  if (textUser.value != '' && textUser.value.length >= 8) {
-    let search = textUser.value
+  if (textUser.value != '') {
+    let cep = textUser.value
     
     const options = {
       method: 'GET',
@@ -11,24 +14,39 @@ function getCep() {
       cache: 'default'
     }
     
-    fetch(`https://viacep.com.br/ws/${search}/json/`, options)
+    fetch(`https://viacep.com.br/ws/${cep}/json/`, options)
     .then(response => {response.json()
-      .then(data => filterData(data))
-    })
+      .then((data) => {
+        if (data.erro === 'true') {
+          result.style.display = 'none'
+          cepInvalid.style.display = 'initial'
+          cepExistent.style.display = 'initial'
+        } else {
+          filterData(data)
+        }
+      })
 
-    .catch(() => console.log('erro'))  
+      result.style.display = 'initial'
+    })
+    .catch(() => {
+      if (cep.length < 8 || cep.length > 8) {
+        cepLength.style.display = 'initial'
+      }  
+
+      result.style.display = 'none'
+      cepInvalid.style.display = 'initial'
+    })
     
-    result.style.display = 'initial'
-  } else {
-    result.style.display = 'none'
+    cepExistent.style.display = 'none'
+    cepLength.style.display = 'none'
+    cepInvalid.style.display = 'none'
   }
 }
 
 function filterData(result) {
   for (const campo in result) {
-    if ( document.querySelector('#' + campo)) {
+    if (document.querySelector('#' + campo)) {
       document.querySelector('#' + campo).value = result[campo]
-      console.log()     
-    }   
+    }  
   }
 }
