2fdf06b80c623d1017c03b65e1f1d4cfb25f3369
p4kz <wolpak@hotmail.com>
2022-04-28 21:31:42 -0300

    fix responsive / fix filter data

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,8 @@
 const textUser = document.querySelector('.text-user')
 const result = document.querySelector('.result-input')
 
-
-const filterData = (result) => {
-  for (const campo in result) {
-    if ( document.querySelector('#' + campo)) {
-      document.querySelector('#' + campo).value = result[campo]
-      console.log()     
-    }   
-  }
-}
-
 function getCep() {
-  if (textUser.value != '' && textUser.value.length > 8) {
+  if (textUser.value != '' && textUser.value.length >= 8) {
     let search = textUser.value
     
     const options = {
@@ -27,9 +17,18 @@ function getCep() {
     })
 
     .catch(() => console.log('erro'))  
-
+    
     result.style.display = 'initial'
   } else {
     result.style.display = 'none'
+  }
+}
+
+function filterData(result) {
+  for (const campo in result) {
+    if ( document.querySelector('#' + campo)) {
+      document.querySelector('#' + campo).value = result[campo]
+      console.log()     
+    }   
   }
 }
