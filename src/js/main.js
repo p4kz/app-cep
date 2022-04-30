847b2b36e78d2329e0bcd1d17d961cf2d1041846
p4kz <wolpak@hotmail.com>
2022-04-30 03:05:19 -0300

    create formatted text and copy button / fix styles

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,10 @@ const result = document.querySelector('.result-input')
 const cepInvalid = document.querySelector('.cep-invalid') 
 const cepLength = document.querySelector('.cep-length')
 const cepExistent = document.querySelector('.cep-existent')
+const formatted = document.querySelector('.formatted')
+const resultBox = document.querySelector('.box-result')
+const msgCopy = document.querySelector('.msg-copy')
+
 
 function getCep() {
   if (textUser.value != '') {
@@ -18,28 +22,33 @@ function getCep() {
     .then(response => {response.json()
       .then((data) => {
         if (data.erro === 'true') {
-          result.style.display = 'none'
-          cepInvalid.style.display = 'initial'
-          cepExistent.style.display = 'initial'
+          display(result, off)
+          display(resultBox, off)
+          display(cepInvalid, on)
+          display(cepExistent, on)
         } else {
           filterData(data)
+          formText()
         }
       })
-
-      result.style.display = 'initial'
+      
+      display(resultBox, on)
+      display(result, on)
     })
     .catch(() => {
       if (cep.length < 8 || cep.length > 8) {
-        cepLength.style.display = 'initial'
+        display(cepLength, on)
       }  
-
-      result.style.display = 'none'
-      cepInvalid.style.display = 'initial'
+      
+      display(resultBox, off)
+      display(result, off)
+      display(cepInvalid, on)
     })
     
-    cepExistent.style.display = 'none'
-    cepLength.style.display = 'none'
-    cepInvalid.style.display = 'none'
+    display(resultBox, off)
+    display(cepExistent, off)
+    display(cepLength, off)
+    display(cepInvalid, off)
   }
 }
 
@@ -47,6 +56,34 @@ function filterData(result) {
   for (const campo in result) {
     if (document.querySelector('#' + campo)) {
       document.querySelector('#' + campo).value = result[campo]
-    }  
+    }
   }
 }
+
+function formText() {
+  if (document.querySelector('#cep').value != '') {
+    const cepForm = document.querySelector('#cep').value
+    const ruaForm = document.querySelector('#logradouro').value
+    const bairroForm = document.querySelector('#bairro').value
+    const cidadeForm = document.querySelector('#localidade').value
+    const estadoForm = document.querySelector('#uf').value
+  
+    formatted.textContent = `${ruaForm}, ${bairroForm}, ${cidadeForm} / ${estadoForm} - ${cepForm}`
+  }
+}
+
+function copy() {
+  navigator.clipboard.writeText(formatted.textContent)
+  msgCopy.style.opacity = '1'
+
+  setTimeout(() => {
+    msgCopy.style.opacity = '0'
+  }, 3000)
+}
+
+function display(tag, text) {
+  tag.style.display = text
+}
+
+let on = 'initial'
+let off = 'none'
